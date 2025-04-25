fb1b3008b56357902d3a596ad1f36307c39b3233
Jeroen <jeroen@vandegeest.eu>
2025-04-25 11:53:43 +0200

    Update WallFollowerRobotcontroller.cpp

--- examples/WallFollower/WallFollowerRobotcontroller.cpp
+++ examples/WallFollower/WallFollowerRobotcontroller.cpp
@@ -16,8 +16,8 @@ WallFollowerRobotcontroller::~WallFollowerRobotcontroller() {
 void WallFollowerRobotcontroller::Setup(Microsim::Robot robot, void *data) {
     this->robot = robot;
     fwdSensor = robot.FindComponent(Guid(4764948050219179759u, 13563840741769542562u)).ToSensor_i32();
-    leftSensor = robot.FindComponent(Guid(5124586844430470358u, 6050595459198776716u)).ToSensor_i32();
-    rightSensor = robot.FindComponent(Guid(5421639628147193954u, 6046799433377730472u)).ToSensor_i32();
+    leftSensor = robot.FindComponent(Guid(5421639628147193954u, 6046799433377730472u)).ToSensor_i32();
+    rightSensor = robot.FindComponent(Guid(5124586844430470358u, 6050595459198776716u)).ToSensor_i32();
 
     motorController = reinterpret_cast<IMotorController*>(new SimulatorMotorController("DefaultMotorController"));
     positionTracker = reinterpret_cast<IPositionTracker*>(new SimulatorPositionTracker("DefaultPositionTracker"));
