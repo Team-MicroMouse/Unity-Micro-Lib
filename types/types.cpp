11049f808fee6499c238aa58f0a198e60bba2db5
Jeroen <jeroen@vandegeest.eu>
2025-05-15 20:57:31 +0200

    pathfinding

--- types/types.cpp
+++ types/types.cpp
@@ -3,6 +3,10 @@
 #include <cmath>
 
 #include "../com/com.h"
+v2i v2i::up() { return v2i(0, 1); }
+v2i v2i::right() { return v2i(1, 0); }
+v2i v2i::down() { return v2i(0, -1); }
+v2i v2i::left() { return v2i(-1, 0); }
 
 v2f v2i::toV2f() const
 {
@@ -17,7 +21,11 @@ float v2i::length() const {
     return std::sqrt(lengthSq());
 }
 
-v2i v2i::operator-(const v2i &b) const {
+v2i v2i::operator+(v2i b) const {
+    return v2i(x + b.x, y + b.y);
+}
+
+v2i v2i::operator-(const v2i b) const {
     return v2i(x - b.x, y - b.y);
 }
 
@@ -34,6 +42,10 @@ v2f v2i::operator/(const float b) const {
         x / b,
         y / b,
     };
+}
+
+bool v2i::operator==(v2i b) const {
+    return x == b.x && y == b.y;
 }
 
 v2f v2f::fromAngle(const int angle) {
@@ -122,4 +134,44 @@ Color Color::green()
 Color Color::blue()
 {
     return Color(0,0,1,1);
+}
+
+bool MapCell::is_discovered() const { return (value & 1 << 0) == 1 << 0; }
+bool MapCell::is_wall_north() const { return (value & 1 << 1) == 1 << 1; }
+bool MapCell::is_wall_east() const { return (value & 1 << 2) == 1 << 2;  }
+bool MapCell::is_wall_south() const { return (value & 1 << 3) == 1 << 3; }
+bool MapCell::is_wall_west() const { return (value & 1 << 4) == 1 << 4; }
+
+void MapCell::set_discovered(bool value) {
+    this->value = (this->value & ~(1 << 0)) | (value << 0);
+}
+
+void MapCell::set_wall_north(bool value) {
+    this->value = (this->value & ~(1 << 1)) | (value << 1);
+}
+
+void MapCell::set_wall_east(bool value) {
+    this->value = (this->value & ~(1 << 2)) | (value << 2);
+}
+
+void MapCell::set_wall_south(bool value) {
+    this->value = (this->value & ~(1 << 3)) | (value << 3);
+}
+
+void MapCell::set_wall_west(bool value) {
+    this->value = (this->value & ~(1 << 4)) | (value << 4);
+}
+
+bool Map::is_in_bounds(v2i position) const
+{
+    return position.x >= 0 && position.y >= 0 && position.x < size.x && position.y < size.y;
+}
+
+MapCell* Map::get_cell(v2i position) const
+{
+    return &cells[position.y * size.x + position.x];
+}
+
+std::size_t v2iHasher::operator()(const v2i& v2i) const {
+    return static_cast<long>(v2i.x) << 32 + static_cast<long>(v2i.y);
 }
