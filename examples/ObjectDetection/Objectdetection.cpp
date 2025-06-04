8bd27f02a3d0482b73bd611f55aa8eb85686c757
Jeroen <jeroen@vandegeest.eu>
2025-06-04 11:41:16 +0200

    Update object detection

--- examples/ObjectDetection/Objectdetection.cpp
+++ examples/ObjectDetection/Objectdetection.cpp
@@ -11,7 +11,13 @@ using namespace Microsim;
 #include <iostream>
 #endif
 
-#define WALL_THRESHOLD 100 // Afstand (in mm) die telt als de muur
+#define WALL_THRESHOLD 400 // Afstand (in mm) die telt als de muur
+#define CURR_CELL_SIZE_F 1000.0f
+
+bool is_wall_of_cell(v2i grid_pos, v2f wall_hit_pos) {
+    const v2i wall_grid_pos = (wall_hit_pos / CURR_CELL_SIZE_F).roundToV2i();
+    return grid_pos == wall_grid_pos;
+}
 
 void Objectdetection::Setup(Microsim::Robot robot, void *data) {
     fwdSensor = robot.FindComponent(Guid(4764948050219179759u, 13563840741769542562u)).ToSensor_i32();
@@ -26,29 +32,52 @@ void Objectdetection::LogSensors(int front, int left, int right) {
 #endif
 
 void Objectdetection::Process(Map map, RobotPosition robot_position) {
-    const v2i grid_pos = (robot_position.position / CELL_SIZE_F).roundToV2i();
+    const v2i grid_pos = (robot_position.position / CURR_CELL_SIZE_F).roundToV2i();
 
-    const v2f fwd_offset = v2f(0, 10);
-    const v2f lhs_offset = v2f(-10, 10);
-    const v2f rhs_offset = v2f(10, 10);
+    const v2f fwd_offset = v2f(0, 100);
+    const v2f lhs_offset = v2f(-100, 100);
+    const v2f rhs_offset = v2f(100, 100);
 
     const v2f fwd_dir = v2f(0,1);
-    const v2f lhs_dir = v2f(-1,1);
-    const v2f rhs_dir = v2f(1,1);
+    const v2f lhs_dir = v2f(-1,0);
+    const v2f rhs_dir = v2f(1,0);
 
-    const float fwd_angle = robot_position.angle;
-    const float lhs_angle = robot_position.angle - 90;
-    const float rhs_angle = robot_position.angle + 90;
+    const float fwd_value = static_cast<float>(fwdSensor.ReadValue());
+    const float lhs_value = static_cast<float>(leftSensor.ReadValue());
+    const float rhs_value = static_cast<float>(rightSensor.ReadValue());
 
-    const int fwd_value = fwdSensor.ReadValue();
-    const int lhs_value = leftSensor.ReadValue();
-    const int rhs_value = rightSensor.ReadValue();
+    const float angle_rad = static_cast<float>(robot_position.angle) * DEG2RAD;
 
-    const v2f fwd_hit_pos = robot_position.position.toV2f() + (fwd_offset + fwd_dir * fwd_value).rotated(fwd_angle * DEG2RAD);
-    const v2f lhs_hit_pos = robot_position.position.toV2f() + (lhs_offset + lhs_dir * lhs_value).rotated(lhs_angle * DEG2RAD);
-    const v2f rhs_hit_pos = robot_position.position.toV2f() + (rhs_offset + rhs_dir * rhs_value).rotated(rhs_angle * DEG2RAD);
+    const v2f fwd_hit_pos = robot_position.position.toV2f() + (fwd_offset + fwd_dir * fwd_value).rotated(angle_rad);
+    const v2f lhs_hit_pos = robot_position.position.toV2f() + (lhs_offset + lhs_dir * lhs_value).rotated(angle_rad);
+    const v2f rhs_hit_pos = robot_position.position.toV2f() + (rhs_offset + rhs_dir * rhs_value).rotated(angle_rad);
 
-    UnityEngine::Debug::DrawLine2D(robot_position.position.toV2f() + fwd_offset.rotated(fwd_angle), fwd_hit_pos, 1, Color::red());
-    UnityEngine::Debug::DrawLine2D(robot_position.position.toV2f() + lhs_offset.rotated(lhs_angle), lhs_hit_pos, 1, Color::green());
-    UnityEngine::Debug::DrawLine2D(robot_position.position.toV2f() + rhs_offset.rotated(rhs_angle), rhs_hit_pos, 1, Color::blue());
+    const v2i fwd_dir_abs = fwd_dir.rotated(angle_rad).explode().normalize().roundToV2i();
+    const v2i lhs_dir_abs = lhs_dir.rotated(angle_rad).explode().normalize().roundToV2i();
+    const v2i rhs_dir_abs = rhs_dir.rotated(angle_rad).explode().normalize().roundToV2i();
+
+    UnityEngine::LogV2f("lhs-offset", fwd_offset.rotated(angle_rad));
+
+    MapCell* cell = map.get_cell(grid_pos);
+
+    if (fwd_value != -1) {
+        UnityEngine::Debug::DrawLine2D((robot_position.position.toV2f() + fwd_offset.rotated(angle_rad)) / CURR_CELL_SIZE_F, fwd_hit_pos / CURR_CELL_SIZE_F, 1, Color::red());
+        cell->set_wall_in_dir(fwd_dir_abs, is_wall_of_cell(grid_pos, fwd_hit_pos));
+    } else {
+        cell->set_wall_in_dir(fwd_dir_abs, false);
+    }
+
+    if (lhs_value != -1) {
+        UnityEngine::Debug::DrawLine2D((robot_position.position.toV2f() + lhs_offset.rotated(angle_rad)) / CURR_CELL_SIZE_F, lhs_hit_pos / CURR_CELL_SIZE_F, 1, Color::green());
+        cell->set_wall_in_dir(lhs_dir_abs, is_wall_of_cell(grid_pos, lhs_hit_pos));
+    } else {
+        cell->set_wall_in_dir(lhs_dir_abs, false);
+    }
+
+    if (rhs_value != -1) {
+        UnityEngine::Debug::DrawLine2D((robot_position.position.toV2f() + rhs_offset.rotated(angle_rad)) / CURR_CELL_SIZE_F, rhs_hit_pos / CURR_CELL_SIZE_F, 1, Color::blue());
+        cell->set_wall_in_dir(rhs_dir_abs, is_wall_of_cell(grid_pos, rhs_hit_pos));
+    } else {
+        cell->set_wall_in_dir(rhs_dir_abs, false);
+    }
 }
